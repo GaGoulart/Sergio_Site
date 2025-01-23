ecd605f58c18d87897a17df67330889e31c3d186
Seu Nome <seu@email.com>
2025-01-23 15:11:01 -0300

    Melhoria do formulario

---
+++ Sergio_Site-main/menu.js
@@ -0,0 +1,32 @@
+const btnMenu = document.getElementById("btn-menu");
+        const btnFechar = document.getElementById("btn-fechar");
+        const menuMobile = document.getElementById("menu-mobile");
+        const overlayMenu = document.getElementById("overlay-menu");
+
+        // Função para abrir o menu
+        function abrirMenu() {
+            menuMobile.style.transform = "translateX(0)";  // Move o menu para a posição visível
+            overlayMenu.style.display = "block";  // Exibe a sobreposição
+        }
+
+        // Função para fechar o menu
+        function fecharMenu() {
+            menuMobile.style.transform = "translateX(100%)";  // Move o menu para fora da tela
+            overlayMenu.style.display = "none";  // Oculta a sobreposição
+        }
+
+        // Eventos de clique
+        btnMenu.addEventListener("click", abrirMenu);  // Abre o menu ao clicar no ícone
+        btnFechar.addEventListener("click", fecharMenu);  // Fecha o menu ao clicar no "X"
+        overlayMenu.addEventListener("click", fecharMenu);  // Fecha o menu ao clicar na sobreposição
+
+        function formatarTelefone(input) {
+            let valor = input.value.replace(/\D/g, ''); // Remove qualquer coisa que não seja número
+            if (valor.length <= 2) {
+                input.value = '(' + valor;
+            } else if (valor.length <= 7) {
+                input.value = '(' + valor.substring(0, 2) + ') ' + valor.substring(2);
+            } else {
+                input.value = '(' + valor.substring(0, 2) + ') ' + valor.substring(2, 7) + '-' + valor.substring(7, 11);
+            }
+        }
